2cfc951e9fbbf03ba6392d1a062c2cad4e6b52ce
emil.abbas@procure.az <emil.abbas@procure.az>
2023-09-20 17:33:22 +0400

    main

---
+++ ProcApi/Data/ProcDatabase/Configurations/ReleaseStrategyConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProcApi.Data.ProcDatabase.Models;
+
+namespace ProcApi.Data.ProcDatabase.Configurations
+{
+    public class ReleaseStrategyConfiguration : IEntityTypeConfiguration<ReleaseStrategy>
+    {
+        public void Configure(EntityTypeBuilder<ReleaseStrategy> builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SeedData()
+        {
+            new List<ReleaseStrategy>
+            {
+                new ReleaseStrategy()
+                {
+
+                },
+                new ReleaseStrategy()
+                {
+
+                }
+            };
+        }
+    }
+}
--- ProcApi/Data/ProcDatabase/Enums/DocumentStatus.cs
+++ ProcApi/Data/ProcDatabase/Enums/DocumentStatus.cs
@@ -3,9 +3,19 @@
 public enum DocumentStatus
 {
     #region Requests
-    PurchaseRequestDraft = 1,
+    PurchaseRequestDraft = 100,
+    PurchaseRequestWaitingFinance = 101,
+    PurchaseRequestApproved = 102,
+    PurchaseRequestCanceled = 103,
+    PurchaseRequestRejected = 104,
+    PurchaseRequestWaitingForHeadOfProcurement = 105,
 
-    ServiceRequestDraft,
+    ServiceRequestDraft = 200,
+    ServiceRequestWaitingFinance = 201,
+    ServiceRequestApproved = 202,
+    ServiceRequestCanceled = 203,
+    ServiceRequestRejected = 204,
+    ServiceRequestWaitingForHeadOfProcurement = 205,
     #endregion
 
     #region Invoices
